a01b6e9ba3dcc58747d0efa6aef44bd31721e54f
rajkumarbhatia <44945506+rajkumarbhatia@users.noreply.github.com>
2020-11-14 15:18:23 +0530

    First Commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Twilio\Rest\Client;
+use Twilio\Jwt\AccessToken;
+use Twilio\Jwt\Grants\VoiceGrant;
+use Twilio\TwiML\VoiceResponse;
+use Twilio\TwiML\Services_Twilio_Twiml;
+use Illuminate\Http\Request;
+use Twilio\Jwt\ClientToken;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(){
+    	return view("index");
+    }
+
+    public function token(Request $request){
+    	$data = $request->all();
+
+    	$twilioAccountSid = env("TWILIO_ACCOUNT_SID");
+		$twilioApiKey = env("TWILIO_API_KEY");
+		$twilioApiSecret = env("TWILIO_API_SECRET");
+
+		// Required for Voice grant
+		$outgoingApplicationSid = env("TWILIO_SID");
+		// An identifier for your app - can be anything you'd like
+		$identity = $data["identity"];
+
+		// Create access token, which we will serialize and send to the client
+		$token = new AccessToken(
+		    $twilioAccountSid,
+		    $twilioApiKey,
+		    $twilioApiSecret,
+		    3600,
+		    $identity
+		);
+
+		// Create Voice grant
+		$voiceGrant = new VoiceGrant();
+		$voiceGrant->setOutgoingApplicationSid($outgoingApplicationSid);
+
+		// Optional: add to allow incoming calls
+		$voiceGrant->setIncomingAllow(true);
+
+		// Add grant to token
+		$token->addGrant($voiceGrant);
+
+		// render token to string
+		// echo $token->toJWT();
+
+		return response()->json([
+			"identity" => $data['identity'],
+			"token" => $token->toJWT() 
+		]);
+
+    }
+
+    public function voice(Request $request){
+
+    	$data = $request->all();
+    	$response = new VoiceResponse();
+		$dial = $response->dial('', ['callerId' => $data["outgoing_caller_id"]]);
+		$dial->client($request->To);
+
+		return $response;
+
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Twilio Client Quickstart</title>
+  <link rel="stylesheet" href="{{ url('public/css/site.css') }}">
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+<body>
+  <div id="dialog-confirm" title="Empty the recycle bin?">
+  <p><span class="ui-icon ui-icon-alert" style="float:left; margin:12px 12px 20px 0;"></span>These items will be permanently deleted and cannot be recovered. Are you sure?</p>
+</div>
+  <div id="controls">
+    <div id="info">
+      <p class="instructions">Twilio Client</p>
+      <div id="client-name"></div>
+      <div id="output-selection">
+        <label>Ringtone Devices</label>
+        <select id="ringtone-devices" multiple></select>
+        <label>Speaker Devices</label>
+        <select id="speaker-devices" multiple></select><br/>
+        <a id="get-devices">Seeing unknown devices?</a>
+      </div>
+    </div>
+    <button id="testToken">Token Get</button>
+    <div id="call-controls">
+      <p class="instructions">Make a Call:</p>
+      <input id="phone-number" type="text" placeholder="Enter a phone # or client name" />
+      <button id="button-call">Call</button>
+      <button id="button-hangup">Hangup</button>
+      <div id="volume-indicators">
+        <label>Mic Volume</label>
+        <div id="input-volume"></div><br/><br/>
+        <label>Speaker Volume</label>
+        <div id="output-volume"></div>
+      </div>
+    </div>
+    <div id="log"></div>
+  </div>
+
+  <script type="text/javascript" src="//media.twiliocdn.com/sdk/js/client/v1.7/twilio.min.js"></script>  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+  <!-- <script src="{{ url('public/js/quickstart.js') }}"></script> -->
+  <script type="text/javascript">
+      $( function() {
+    $( "#dialog-confirm" ).dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      buttons: {
+        "Delete all items": function() {
+          $( this ).dialog( "close" );
+        },
+        Cancel: function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  } );
+
+  </script>
+</body>
+</html>
